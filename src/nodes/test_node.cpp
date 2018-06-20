881bb9d002f1df1d33ec3fbeee9e15db29e4bbdc
Artem Kovalev <leon112rus@gmail.com>
2018-06-20 18:21:37 +0300

    Send rpm commands test

--- src/nodes/test_node.cpp
+++ src/nodes/test_node.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cstdlib>
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/Int16MultiArray.h>
 
 void twist_cb(const geometry_msgs::Twist::ConstPtr& twist_msg)
 {
@@ -15,9 +17,6 @@ void twist_cb(const geometry_msgs::Twist::ConstPtr& twist_msg)
     ay = twist_msg->angular.y;
     az = twist_msg->angular.z;
 
-    ROS_INFO("TWISTING");
-    ROS_INFO("Linear: [%f] [%f] [%f]", vx, vy, vz);
-    ROS_INFO("Angular: [%f] [%f] [%f]", ax, ay, az);
 }
 
 int main(int argc, char** argv)
@@ -25,12 +24,29 @@ int main(int argc, char** argv)
     try {
         ros::init(argc, argv, "test_node_listener");
         ros::NodeHandle n;
-        ros::Subscriber sub = n.subscribe("turtle1/cmd_vel", 10, twist_cb);
+        ros::Subscriber sub = n.subscribe("cmd_vel", 16, twist_cb);
+        ros::Publisher pub = n.advertise<std_msgs::Int16MultiArray>("cmd_rpm", 100);
 
         ros::Rate rate(30);
 
-        ROS_INFO("Privyet, I am test echo node");
         while (ros::ok()) {
+            std_msgs::Int16MultiArray arr_msg;
+
+            arr_msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
+            arr_msg.layout.dim[0].label = "rpm";
+            arr_msg.layout.dim[0].size = 2;
+            arr_msg.layout.dim[0].stride = 2;
+            if (argc > 1) {
+
+                arr_msg.data.push_back(strtol(argv[1], nullptr, 10));
+                arr_msg.data.push_back(strtol(argv[2], nullptr, 10));
+            } else {
+                arr_msg.data.push_back(0);
+                arr_msg.data.push_back(0);
+            }
+
+            pub.publish(arr_msg);
+
             ros::spinOnce();
             rate.sleep();
         }
