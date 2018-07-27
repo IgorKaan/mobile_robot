87a4756c78abc2fffff71267490d8a85dde1c921
Artem Kovalev <leon112rus@gmail.com>
2018-07-27 19:29:43 +0300

    Add navigation stack config and amcl.launch

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,14 @@ int main() {
 
     M_2_PI;
 
+    // Record rosbag
+    // rosbag record -O mylaserdata /base_scan /tf
+    // before playing
+    // rosparam set use_sim_time true
+    // rosbag play --clock <name of the bag>
+    // Or just save the map after mapping
+    // rosrun map_server map_saver -f <map_name>
+
 
     return 0;
 }
