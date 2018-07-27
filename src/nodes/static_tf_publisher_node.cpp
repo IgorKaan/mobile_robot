d3ff7014183cff364da26b1929118dc1484d84df
Artem Kovalev <leon112rus@gmail.com>
2018-07-27 18:18:16 +0300

    Fix laser transform

--- src/nodes/static_tf_publisher_node.cpp
+++ src/nodes/static_tf_publisher_node.cpp
@@ -11,9 +11,9 @@ int main(int argc, char** argv)
 
     tf::TransformBroadcaster broadcaster;
 
-    constexpr float lidar_x = -0.00651f; + 0.07329f;
+    constexpr float lidar_x = -0.07329f + 0.00651f;
     constexpr float lidar_y = 0.0f;
-    constexpr float lidar_z = 0.03909f;
+    constexpr float lidar_z = 0.03909f + 0.02791f;
 
     tf::Quaternion lidar_quat;
     lidar_quat.setRPY(0.0, 0.0, -M_PI);
