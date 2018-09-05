54da59343caeb8f5902d27ec598f0bf7ebe1a645
Artem Kovalev <leon112rus@gmail.com>
2018-09-05 16:09:49 +0300

    Add base_predictor corrector

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,25 @@
 #include <termios.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <state_estimation/base_predictor_corrector.h>
 
 #include "kinematics/differential_drive.h"
 
 int main() {
+    base_predictor_corrector<6, 3, 3>::state_vector vec;
+
+    for (int i = 0; i < vec.rows(); i++) {
+        vec(i) = i * 3.14f;
+    }
+
+    base_predictor_corrector<6, 3, 3>::state_container states;
+
+    states.push_back(vec);
+    states.push_back(vec);
+
+    for (const auto& state : states) {
+        std::cout << state << std::endl;
+    }
 
     return 0;
 }
