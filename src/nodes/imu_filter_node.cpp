6fd1656b5579a06c059a8cd096929e50b6a9f81c
Artem Kovalev <leon112rus@gmail.com>
2020-02-13 16:36:59 +0300

    Add imu filter node

---
+++ src/nodes/imu_filter_node.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cstdlib>
+#include <mutex>
+
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <geometry_msgs/Vector3.h>
+
+class imu_filter_node {
+public:
+    imu_filter_node(const std::string& gyro_topic, const std::string& accel_topic)
+        : m_nh("imu_filter_node")
+    {
+        m_pub = m_nh.advertise<sensor_msgs::Imu>("/imu", 10);
+
+        ros::Subscriber gyro_sub = m_nh.subscribe<geometry_msgs::Vector3>(gyro_topic, 10, &imu_filter_node::gyro_callback, this);
+        ros::Subscriber accel_sub = m_nh.subscribe<geometry_msgs::Vector3>(gyro_topic, 10, &imu_filter_node::accel_callback, this);
+    }
+
+    void gyro_callback(const geometry_msgs::Vector3 gyro_msg) {
+        std::lock_guard<std::mutex> guard(m_mutex);
+
+        m_gyro_msg.x = -gyro_msg.y;
+        m_gyro_msg.y = -gyro_msg.x;
+        m_gyro_msg.z = -gyro_msg.z;
+    }
+
+    void accel_callback(const geometry_msgs::Vector3 accel_msg) {
+        std::lock_guard<std::mutex> guard(m_mutex);
+
+        m_accel_msg.x = -accel_msg.y;
+        m_accel_msg.y = -accel_msg.x;
+        m_accel_msg.z = -accel_msg.z;
+    }
+
+    void imu_filter_update()
+    {
+        std::lock_guard<std::mutex> guard(m_mutex);
+        sensor_msgs::Imu imu_msg;
+
+        imu_msg.header.frame_id = "base_link";
+        imu_msg.header.stamp = ros::Time::now();
+        imu_msg.orientation_covariance.elems[0] = -1;
+
+        imu_msg.angular_velocity = m_gyro_msg;
+        imu_msg.angular_velocity_covariance.elems[0] = 0.1;
+        imu_msg.angular_velocity_covariance.elems[4] = 0.1;
+        imu_msg.angular_velocity_covariance.elems[8] = 0.1;
+
+        imu_msg.linear_acceleration = m_accel_msg;
+        imu_msg.linear_acceleration_covariance.elems[0] = 0.1;
+        imu_msg.linear_acceleration_covariance.elems[4] = 0.1;
+        imu_msg.linear_acceleration_covariance.elems[8] = 0.1;
+
+        m_pub.publish(imu_msg);
+    }
+private:
+    ros::NodeHandle m_nh;
+    ros::Publisher m_pub;
+
+    geometry_msgs::Vector3 m_gyro_msg, m_accel_msg;
+
+    std::mutex m_mutex;
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "imu_filter_node");
+
+    imu_filter_node filter("/gyro", "/accel");
+
+    ROS_INFO("Starting starting imu filter node");
+    ros::Rate rate(100.0f);
+    while (ros::ok()) {
+        filter.imu_filter_update();
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
+
