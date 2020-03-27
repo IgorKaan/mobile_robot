189d021ffbccd25ad0720990d4c459ef8f259d72
Artem Kovalev <leon112rus@gmail.com>
2020-03-27 14:51:24 +0300

    Setup autonomous navigation

--- src/nodes/static_tf_publisher_node.cpp
+++ src/nodes/static_tf_publisher_node.cpp
@@ -17,7 +17,8 @@ int main(int argc, char** argv)
     constexpr float lidar_y = 0.0f;
     constexpr float lidar_z = 0.03909f + 0.02791f;
     tf::Quaternion lidar_quat;
-    lidar_quat.setRPY(0.0, 0.0, -M_PI);
+    //lidar_quat.setRPY(0.0, 0.0, -M_PI);
+    lidar_quat.setRPY(0.0, 0.0, 0.0);
 
     while (n.ok()) {
         broadcaster.sendTransform(
