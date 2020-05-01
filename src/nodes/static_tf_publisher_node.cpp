9f4e6eac40253e702d29d10d27785e6e0f04fe60
Artem Kovalev <leon112rus@gmail.com>
2020-05-01 15:19:47 +0300

    Add additional sensor frames

--- src/nodes/static_tf_publisher_node.cpp
+++ src/nodes/static_tf_publisher_node.cpp
@@ -1,6 +1,6 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
-
+#include <cmath>
 
 
 int main(int argc, char** argv)
@@ -25,6 +25,69 @@ int main(int argc, char** argv)
     tf::Quaternion lidar_rear_quat;
     lidar_rear_quat.setRPY(0.0, 0.0, 0.0);
 
+    /*
+     * FRONT LEFT
+     */
+    const tf::Vector3 sensor_fl_0(0.2307, 0.0638, 0.0f);
+    tf::Quaternion sensor_fl_0_quat;
+    const float sensor_fl0_angle = 0.0;
+    sensor_fl_0_quat.setRPY(0.0, 0.0, sensor_fl0_angle);
+
+    const tf::Vector3 sensor_fl_1(0.2175, 0.1047, 0.0f);
+    tf::Quaternion sensor_fl_1_quat;
+    const float sensor_fl1_angle = std::atan2(sensor_fl_1.y(), sensor_fl_1.x());
+    sensor_fl_1_quat.setRPY(0.0, 0.0, sensor_fl1_angle);
+
+    const tf::Vector3 sensor_fl_2(0.1987, 0.1347, 0.0f);
+    tf::Quaternion sensor_fl_2_quat;
+    const float sensor_fl2_angle = std::atan2(sensor_fl_2.y(), sensor_fl_2.x());
+    sensor_fl_2_quat.setRPY(0.0, 0.0, sensor_fl2_angle);
+
+    /*
+     * FRONT RIGHT
+     */
+    const tf::Vector3 sensor_fr_0(sensor_fl_0.x(), -sensor_fl_0.y(), sensor_fl_0.z());
+    tf::Quaternion sensor_fr_0_quat;
+    sensor_fr_0_quat.setRPY(0.0, 0.0, -sensor_fl0_angle);
+
+    const tf::Vector3 sensor_fr_1(sensor_fl_1.x(), -sensor_fl_1.y(), sensor_fl_1.z());
+    tf::Quaternion sensor_fr_1_quat;
+    sensor_fr_1_quat.setRPY(0.0, 0.0, -sensor_fl1_angle);
+
+    const tf::Vector3 sensor_fr_2(sensor_fl_2.x(), -sensor_fl_2.y(), sensor_fl_2.z());
+    tf::Quaternion sensor_fr_2_quat;
+    sensor_fr_2_quat.setRPY(0.0, 0.0, -sensor_fl2_angle);
+
+    /*
+     * REAR LEFT
+     */
+    const tf::Vector3 sensor_rl_0(-sensor_fl_0.x(), sensor_fl_0.y(), sensor_fl_0.z());
+    tf::Quaternion sensor_rl_0_quat;
+    sensor_rl_0_quat.setRPY(0.0, 0.0, M_PI-sensor_fl0_angle);
+
+    const tf::Vector3 sensor_rl_1(-sensor_fl_1.x(), sensor_fl_1.y(), sensor_fl_1.z());
+    tf::Quaternion sensor_rl_1_quat;
+    sensor_rl_1_quat.setRPY(0.0, 0.0, M_PI-+sensor_fl1_angle);
+
+    const tf::Vector3 sensor_rl_2(-sensor_fl_2.x(), sensor_fl_2.y(), sensor_fl_2.z());
+    tf::Quaternion sensor_rl_2_quat;
+    sensor_rl_2_quat.setRPY(0.0, 0.0, M_PI-sensor_fl2_angle);
+
+    /*
+     * REAR RIGHT
+     */
+    const tf::Vector3 sensor_rr_0(-sensor_fl_0.x(), -sensor_fl_0.y(), sensor_fl_0.z());
+    tf::Quaternion sensor_rr_0_quat;
+    sensor_rr_0_quat.setRPY(0.0, 0.0, M_PI+sensor_fl0_angle);
+
+    const tf::Vector3 sensor_rr_1(-sensor_fl_1.x(), -sensor_fl_1.y(), sensor_fl_1.z());
+    tf::Quaternion sensor_rr_1_quat;
+    sensor_rr_1_quat.setRPY(0.0, 0.0, M_PI+sensor_fl1_angle);
+
+    const tf::Vector3 sensor_rr_2(-sensor_fl_2.x(), -sensor_fl_2.y(), sensor_fl_2.z());
+    tf::Quaternion sensor_rr_2_quat;
+    sensor_rr_2_quat.setRPY(0.0, 0.0, M_PI+sensor_fl2_angle);
+
     while (n.ok()) {
         broadcaster.sendTransform(
                 tf::StampedTransform(
@@ -34,6 +97,39 @@ int main(int argc, char** argv)
                 tf::StampedTransform(
                         tf::Transform(lidar_rear_quat, tf::Vector3(lidar_rear_x, lidar_rear_y, lidar_rear_z)),
                         ros::Time::now(), "base_link", "laser_rear"));
+        broadcaster.sendTransform(
+                tf::StampedTransform(
+                        tf::Transform(lidar_rear_quat, tf::Vector3(0.0, 0.0, lidar_front_z)),
+                        ros::Time::now(), "base_link", "laser_merged"));
+
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_fl_0_quat, sensor_fl_0),
+                                                       ros::Time::now(), "base_link", "sensor_frame_fl_0"));
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_fl_1_quat, sensor_fl_1),
+                                                       ros::Time::now(), "base_link", "sensor_frame_fl_1"));
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_fl_2_quat, sensor_fl_2),
+                                                       ros::Time::now(), "base_link", "sensor_frame_fl_2"));
+
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_fr_0_quat, sensor_fr_0),
+                                                       ros::Time::now(), "base_link", "sensor_frame_fr_0"));
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_fr_1_quat, sensor_fr_1),
+                                                       ros::Time::now(), "base_link", "sensor_frame_fr_1"));
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_fr_2_quat, sensor_fr_2),
+                                                       ros::Time::now(), "base_link", "sensor_frame_fr_2"));
+
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_rl_0_quat, sensor_rl_0),
+                                                       ros::Time::now(), "base_link", "sensor_frame_rl_0"));
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_rl_1_quat, sensor_rl_1),
+                                                       ros::Time::now(), "base_link", "sensor_frame_rl_1"));
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_rl_2_quat, sensor_rl_2),
+                                                       ros::Time::now(), "base_link", "sensor_frame_rl_2"));
+
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_rr_0_quat, sensor_rr_0),
+                                                       ros::Time::now(), "base_link", "sensor_frame_rr_0"));
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_rr_1_quat, sensor_rr_1),
+                                                       ros::Time::now(), "base_link", "sensor_frame_rr_1"));
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform(sensor_rr_2_quat, sensor_rr_2),
+                                                       ros::Time::now(), "base_link", "sensor_frame_rr_2"));
+
         r.sleep();
     }
 
